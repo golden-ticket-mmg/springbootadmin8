2ccf494a4eb2d055f958259c518e868086dbc533
muraliweb9 <muraliweb@hotmail.com>
2023-09-05 10:20:03 +0100

    Initial

---
+++ src/main/java/com/hackearth/goldenticket/mmg/springbootadmin8/Springbootadmin8Application.java
@@ -0,0 +1,15 @@
+package com.hackearth.goldenticket.mmg.springbootadmin8;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAdminServer
+@SpringBootApplication
+public class Springbootadmin8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springbootadmin8Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hackearth/goldenticket/mmg/springbootadmin8/Springbootadmin8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackearth.goldenticket.mmg.springbootadmin8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootadmin8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
